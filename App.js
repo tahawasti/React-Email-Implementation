7c36801c2210d123cc6ce473d6ed31f973d1c96f
Syed Taha Nadeem Wasti <63840529+tahawasti@users.noreply.github.com>
2020-05-16 16:44:53 +1000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ContactComp from "./component/contactComp";
+
+function App() {
+  return <ContactComp />;
+}
+
+export default App;
